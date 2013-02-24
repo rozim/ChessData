594bc68e3353cc0283b45164f3f05653f43a3fd5
Dave Spencer <david.spencerian@gmail.com>
2013-02-24 15:37:11 -0800

    new fix.py and new pgn

---
+++ Twic/README.md
@@ -0,0 +1,2 @@
+
+http://www.theweekinchess.com/twic
---
+++ Twic/fix.py
@@ -0,0 +1,22 @@
+
+# get rid of comment lines with two dashes
+# that TWIC sometimes has
+
+import sys, os
+
+def doit(fin, fout):
+  ar = [line for line in fin.readlines()]
+  rm = 0
+  for i in range(len(ar) - 1):
+    if ar[i + 1].startswith('--'):
+      rm += 1
+      ar[i] = ar[i + 1] = None
+      i += 1
+  for line in (ent for ent in ar if ent is not None):
+    fout.write(line)
+  return  rm
+
+
+for fn in sys.argv[1:]:
+  rm = doit(file(fn), file('fix-' + fn, 'w'))
+  print fn, rm
