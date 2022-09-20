6df7a84785ccb1a55b0062c7be11f37c0cb30cd1
Dave Spencer <david.spencerian@gmail.com>
2022-09-19 19:25:10 -0700

    seems to be working

--- Code/fen-analysis-db.py
+++ Code/fen-analysis-db.py
@@ -8,49 +8,25 @@ import sqlitedict
 import chess
 from chess import WHITE, BLACK
 import chess.engine
-import chess.pgn
 
 from absl import app
 from absl import flags
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('pgn', None, 'Input')
+flags.DEFINE_string('fen', None, 'FEN input file')
 flags.DEFINE_string('output', None, 'Output sqlite')
 flags.DEFINE_string('reference', None, 'Reference sqlite')
 flags.DEFINE_integer('depth', 1, 'Max depth')
 
-flags.mark_flag_as_required('pgn')
+flags.mark_flag_as_required('fen')
 flags.mark_flag_as_required('output')
 
-# FN = 'sinqcup22.pgn'
-# FEN = 'rn2kbnr/ppq2pp1/4p3/2pp2Bp/2P4P/1Q6/P2NNPP1/3RK2R w Kkq - 2 13'
-
 MULTIPV = 50
-MIN_PLY = 20
-MAX_PLY = 120
-
-# MAX_DEPTH = 12
-MAX_GAMES = 0
 
 HASH = 512
 THREADS = 1 # reproducible
 
-COMMIT_FREQ = 5 # games
-
-def gen_games(fn):
-  f = open(fn, 'r', encoding='utf-8', errors='replace'))
-  while True:
-    g = chess.pgn.read_game(f)
-    if g is None:
-      return
-    yield g
-
-
-def gen_moves(game):
-  board = game.board()
-  for move in game.mainline_moves():
-    board.push(move)
-    yield move, board
+COMMIT_FREQ_SECS = 300.0
 
 
 def simplify_pv(pv):
@@ -77,14 +53,9 @@ def simplify_multi(multi, board):
              'pv': simplify_pv(pv)}
 
 
-def simplify_fen(board):
-  #rn2kbnr/ppq2pp1/4p3/2pp2Bp/2P4P/1Q6/P2NNPP1/3RK2R w Kkq - 2 13
-  return ' '.join(board.fen().split(' ')[0:4])
-
-
 def main(argv):
   del argv
-  assert os.access(FLAGS.pgn, os.R_OK)
+  assert os.access(FLAGS.fen, os.R_OK)
 
   reference = set()
   if FLAGS.reference:
@@ -101,53 +72,69 @@ def main(argv):
     rdb.close()
     del rdb
 
+  print(f'Open {FLAGS.fen}')
+  fens = open(FLAGS.fen, 'r').read().split('\n')
+  fens = [fen.strip() for fen in fens]
+  print('FENS: ', len(fens))
+
   ncache = 0
   ncache_ref = 0
   nwrite = 0
-  engine = chess.engine.SimpleEngine.popen_uci('stockfish')
+  engine = chess.engine.SimpleEngine.popen_uci('./stockfish')
   engine.configure({"Hash": HASH})
   engine.configure({"Threads": THREADS})
   #t_special = time.time() + 60.0
 
   print(f'Db: {FLAGS.output}')
-  db = sqlitedict.open(FLAGS.output, 'c',
+  db = sqlitedict.open(FLAGS.output,
+                       flag='c',
                        encode=json.dumps,
                        decode=json.loads,
                        timeout=10)
 
-  print(f'Open {FLAGS.pgn}')
-  for gnum, game in enumerate(gen_games(FLAGS.pgn)):
-    if gnum % COMMIT_FREQ == 0:
-      db.commit()
-    # if t_special >= time.time():
-    #   print('commit ', ncache, ncache_ref, ' write ', nwrite)
-    #   t_special = time.time() + 60.0
 
-    if MAX_GAMES > 0 and gnum >= MAX_GAMES:
-      break
-    headers = game.headers
+  t_last_commit = time.time()
+  max_dt = 0.0
+  max_nodes = 0
+  t_start = time.time()
+
+  for nfen, fen in enumerate(fens):
+    if len(fen) == 0:
+      continue
+
+    now = time.time()
+    if now >= (t_last_commit + COMMIT_FREQ_SECS):
+      t1 = time.time()
+      db.commit()
+      t_last_commit = now
 
     t1 = time.time()
-    for ply, (move, board) in enumerate(gen_moves(game)):
-      if ply < MIN_PLY or ply > MAX_PLY:
+    engine.configure({"Clear Hash": None})
+    nodes = 0
+    board = chess.Board(fen)
+
+    for depth in range(FLAGS.depth + 1):
+      sfen = f'{fen}|{depth}'
+      if sfen in reference:  # In memory
+        ncache_ref += 1
         continue
-
-      engine.configure({"Clear Hash": None})
-      multi = None
-      for depth in range(FLAGS.depth + 1):
-        sfen = f'{simplify_fen(board)}|{depth}'
-        if sfen in reference:  # In memory
-          ncache_ref += 1
-          continue
-        if sfen in db:  # Hmm, maybe should be in memory
-          ncache += 1
-          continue
-        multi = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=MULTIPV)
-        db[sfen] = list(simplify_multi(multi, board))
-        nwrite += 1
-
+      if sfen in db:  # Hmm, maybe should be in memory
+        ncache += 1
+        continue
+      multi = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=MULTIPV)
+      if len(multi) > 0:
+        nodes += multi[0].get('nodes', 0)
+      db[sfen] = list(simplify_multi(multi, board))
+      nwrite += 1
     dt = time.time() - t1
-    print(f'{gnum:4d} {headers["White"]:24s} - {headers["Black"]:24s} : {headers["Result"]:7.7s} ply={ply:3d} dt={dt:.1f}s')
+    star = ''
+    if dt > max_dt:
+      max_dt = dt
+      star += 't'
+    if nodes > max_nodes:
+      max_nodes = nodes
+      star += '*'
+    print(f'{nfen:4d} {int(100.0*nfen/len(fens))}% FEN: {fen} {dt:.1f}s {nodes} {star}')
 
   print('Cache: ', ncache)
   print('Cache: ', ncache_ref, '(reference db)')
