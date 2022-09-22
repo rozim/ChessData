e0ae56c9d4f4a31b42547cd89e7bd26babb0b07c
Dave Spencer <david.spencerian@gmail.com>
2022-09-22 10:58:40 -0700

    fn

--- Code/run-fen-analysis-db.sh
+++ Code/run-fen-analysis-db.sh
@@ -4,5 +4,5 @@ set -e
 seq 0 9 | \
     xargs -P 4 -I % -t python -u fen-analysis-db.py --fen=../Analysis/$1_%.txt \
 	  --output=../Analysis/pv1/$1_%.sqlite \
-	  --reference=../Analysis/pv1/mega.sqlite > charlotte.txt \
+	  --reference=../Analysis/pv1/mega.sqlite > $1.txt \
 	  --depth=16
