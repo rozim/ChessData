926429115afe0bfcada289cca88380850e686092
Dave Spencer <david.spencerian@gmail.com>
2022-09-28 23:30:17 -0700

    prelim version seems to be working

--- Code/analyze-pgn.py
+++ Code/analyze-pgn.py
@@ -22,12 +22,14 @@ flags.DEFINE_string('pgn', None, 'PGN input file')
 flags.DEFINE_string('engine', 'stockfish', '')
 flags.DEFINE_integer('min_ply', 20, 'Starting ply')
 flags.DEFINE_integer('max_ply', 120, 'Ending ply')
-flags.DEFINE_string('db', '', 'Optional database name')
+flags.DEFINE_string('db', None, 'Optional database name')
 
-flags.DEFINE_integer('depth', 1, '')
+flags.DEFINE_integer('min_depth', 0, '')
+flags.DEFINE_integer('max_depth', 10, '')
 flags.DEFINE_integer('multipv', 1, '')
 
 flags.mark_flag_as_required('pgn')
+flags.mark_flag_as_required('db')
 
 HASH = 512
 THREADS = 1
@@ -36,7 +38,7 @@ THREADS = 1
 def main(_argv):
   assert os.access(FLAGS.pgn, os.R_OK)
   assert FLAGS.multipv >= 1
-  db = None
+
   if FLAGS.db:
     print(f'Opening {FLAGS.db}')
     db = sqlitedict.open(filename=FLAGS.db,
@@ -45,44 +47,45 @@ def main(_argv):
                          decode=json.loads)
     print(f'Rows {len(db)}')
 
-  engine = chess.engine.SimpleEngine.popen_uci('./stockfish')
+  engine = chess.engine.SimpleEngine.popen_uci(FLAGS.engine)
   engine.configure({"Hash": HASH})
   engine.configure({"Threads": THREADS})
+
   n_write = 0
   t0 = time.time()
-  for game in gen_games(FLAGS.pgn):
+
+  for gnum, game in enumerate(gen_games(FLAGS.pgn)):
+    t1 = time.time()
+    headers = game.headers
+    white = headers['White']
+    black = headers['Black']
     for uci, san, ply, board in gen_moves(game):
+      if len(list(board.legal_moves)) == 1:
+        continue # Forced, only move
+      if ply < FLAGS.min_ply or (FLAGS.max_ply and ply > FLAGS.max_ply):
+        continue
+
       engine.configure({"Clear Hash": None})
       sfen = simplify_fen(board)
-      key = f'{sfen}|{FLAGS.depth}'
-      t1 = time.time()
-      try:
-        multi = db[key]
-      except (KeyError, TypeError):
-        multi = engine.analyse(board, chess.engine.Limit(depth=FLAGS.depth), multipv=FLAGS.multipv)
-        multi = list(simplify_multi(multi, board))
 
-        if FLAGS.db:
+      for depth in range(FLAGS.min_depth, FLAGS.max_depth):
+        key = f'{sfen}|{depth}'
+        try:
+          multi = db[key]
+        except KeyError:
+          multi = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=FLAGS.multipv)
+          multi = list(simplify_multi(multi, board))
+
           db[key] = multi
           n_write += 1
+    dt = time.time() - t0
+    print(f'{gnum:4d} {white:24s} {black:24s} {dt:.1f}s')
 
-      dt = time.time() - t1
-
-      if FLAGS.multipv == 1:
-        ev = multi[0]['ev']
-        nodes = multi[0]['nodes']
-        pv = multi[0]['pv']
-        pv_san = board.san(chess.Move.from_uci(pv[0]))
-        print(f'{ply:3d} {san:8s} {pv_san:8s} {ev:8d} {nodes:8d} {dt:4.1f}s ')
-      else:
-        print(ply, san, f'{dt:.1f}s')
-        for m in multi:
-          print('\t', m)
   dt = time.time() - t0
   print(f'Total {dt:.1f}s')
-  if FLAGS.db:
-    print(f'N_Write: {n_write}')
-    db.commit()
+  print(f'Writes: {n_write}')
+
+  db.commit()
   engine.quit()
 
 
