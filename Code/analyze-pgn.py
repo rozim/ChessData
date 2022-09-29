8da5d884f1a32cbf28994a756a2894896d3fc8e0
Dave Spencer <david.spencerian@gmail.com>
2022-09-29 16:16:06 -0700

    more nice output stats

--- Code/analyze-pgn.py
+++ Code/analyze-pgn.py
@@ -51,16 +51,17 @@ def main(_argv):
   engine.configure({"Hash": HASH})
   engine.configure({"Threads": THREADS})
 
-  n_write = 0
+  n_write, n_cache = 0, 0
   t0 = time.time()
 
-  for gnum, game in enumerate(gen_games(FLAGS.pgn)):
+  for gnum, (game, pct) in enumerate(gen_games(FLAGS.pgn)):
     t1 = time.time()
     headers = game.headers
     white = headers['White']
     black = headers['Black']
     result = headers['Result']
     for uci, san, ply, board in gen_moves(game):
+      actions = 0
       if len(list(board.legal_moves)) == 1:
         continue # Forced, only move
       if ply < FLAGS.min_ply or (FLAGS.max_ply and ply > FLAGS.max_ply):
@@ -73,20 +74,23 @@ def main(_argv):
         key = f'{sfen}|{depth}'
         try:
           multi = db[key]
+          n_cache += 1
         except KeyError:
           multi = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=FLAGS.multipv)
           multi = list(simplify_multi(multi, board))
 
           db[key] = multi
           n_write += 1
+          actions += 1
     db.commit()
     dt = time.time() - t1
-    print(f'{gnum:4d} {white:24s} {black:24s} {result:8s} {dt:.1f}s')
+    print(f'{(100.0 * pct):3.1f}% {gnum:4d} {white:24s} {black:24s} {result:8s} (c:{n_cache:8d} w:{n_write:8d}) p:{ply:3d} a/dt:{(actions/dt):.1f}s dt:{dt:.1f}s')
 
   dt = time.time() - t0
   print()
   print(f'Total {dt:.1f}s')
   print(f'Writes: {n_write}')
+  print(f'Caches: {n_cache}')
 
   db.commit()
   engine.quit()
