87da4c379adb02a9ae9619a8b5969c7079bc289f
Dave Spencer <david.spencerian@gmail.com>
2022-09-29 11:19:26 -0700

    minply, stockfish

--- Code/analyze-game.py
+++ Code/analyze-game.py
@@ -1,7 +1,8 @@
+# Analyze a PGN file to a single depth.
+import json
 import logging
-import sys
 import os
-
+import sys
 import time
 
 import chess
@@ -12,13 +13,14 @@ import chess.engine
 from absl import app
 from absl import flags
 
+import sqlitedict
+
 from util import *
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('pgn', None, 'PGN input file')
 flags.DEFINE_string('engine', 'stockfish', '')
-flags.DEFINE_integer('min_ply', 20, 'Starting ply')
-flags.DEFINE_integer('max_ply', 120, 'Ending ply')
+flags.DEFINE_string('db', '', 'Optional database name')
 
 flags.DEFINE_integer('depth', 1, '')
 flags.DEFINE_integer('multipv', 1, '')
@@ -32,19 +34,36 @@ THREADS = 1
 def main(_argv):
   assert os.access(FLAGS.pgn, os.R_OK)
   assert FLAGS.multipv >= 1
+  db = None
+  if FLAGS.db:
+    print(f'Opening {FLAGS.db}')
+    db = sqlitedict.open(filename=FLAGS.db,
+                         flag='c',
+                         encode=json.dumps,
+                         decode=json.loads)
+    print(f'Rows {len(db)}')
 
-  engine = chess.engine.SimpleEngine.popen_uci('./stockfish')
+  engine = chess.engine.SimpleEngine.popen_uci(FLAGS.engine)
   engine.configure({"Hash": HASH})
   engine.configure({"Threads": THREADS})
-  #print(engine.options)
+  n_write = 0
   t0 = time.time()
   for game in gen_games(FLAGS.pgn):
     for uci, san, ply, board in gen_moves(game):
-
       engine.configure({"Clear Hash": None})
+      sfen = simplify_fen(board)
+      key = f'{sfen}|{FLAGS.depth}'
       t1 = time.time()
-      multi = engine.analyse(board, chess.engine.Limit(depth=FLAGS.depth), multipv=FLAGS.multipv)
-      multi = list(simplify_multi(multi, board))
+      try:
+        multi = db[key]
+      except (KeyError, TypeError):
+        multi = engine.analyse(board, chess.engine.Limit(depth=FLAGS.depth), multipv=FLAGS.multipv)
+        multi = list(simplify_multi(multi, board))
+
+        if FLAGS.db:
+          db[key] = multi
+          n_write += 1
+
       dt = time.time() - t1
 
       if FLAGS.multipv == 1:
@@ -59,6 +78,9 @@ def main(_argv):
           print('\t', m)
   dt = time.time() - t0
   print(f'Total {dt:.1f}s')
+  if FLAGS.db:
+    print(f'N_Write: {n_write}')
+    db.commit()
   engine.quit()
 
 
