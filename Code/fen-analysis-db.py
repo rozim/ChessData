026e2fa4639fd54833af5edfd5417897ef24fed0
Dave Spencer <david.spencerian@gmail.com>
2022-09-30 22:28:51 -0700

    misc

--- Code/fen-analysis-db.py
+++ Code/fen-analysis-db.py
@@ -1,6 +1,8 @@
+import logging
 import sys
 import os
 import json
+import random
 
 import time
 import sqlitedict
@@ -53,8 +55,18 @@ def simplify_multi(multi, board):
              'pv': simplify_pv(pv)}
 
 
+def open_engine():
+  engine = chess.engine.SimpleEngine.popen_uci('./stockfish')
+  engine.configure({"Hash": HASH})
+  engine.configure({"Threads": THREADS})
+  return engine
+
+
 def main(argv):
   del argv
+  logging.basicConfig(level=logging.DEBUG)
+  logging.getLogger().setLevel(logging.DEBUG)
+
   assert os.access(FLAGS.fen, os.R_OK)
 
   reference = set()
@@ -75,28 +87,33 @@ def main(argv):
   print(f'Open {FLAGS.fen}')
   fens = open(FLAGS.fen, 'r').read().split('\n')
   fens = [fen.strip() for fen in fens]
+  if True:
+    # Stockfish keeps hanging -- let's mix things up at least.
+    random.shuffle(fens)
   print('FENS: ', len(fens))
 
   ncache = 0
   ncache_ref = 0
   nwrite = 0
-  engine = chess.engine.SimpleEngine.popen_uci('./stockfish')
-  engine.configure({"Hash": HASH})
-  engine.configure({"Threads": THREADS})
+
+
   #t_special = time.time() + 60.0
 
   print(f'Db: {FLAGS.output}')
   db = sqlitedict.open(FLAGS.output,
                        flag='c',
                        encode=json.dumps,
-                       decode=json.loads,
-                       timeout=10)
+                       decode=json.loads)
+
+  reference.update(list(db.keys()))
 
 
   t_last_commit = time.time()
   max_dt = 0.0
   max_nodes = 0
   t_start = time.time()
+  #engine = open_engine()
+  #engine = None
 
   for nfen, fen in enumerate(fens):
     if len(fen) == 0:
@@ -113,9 +130,8 @@ def main(argv):
     nodes = 0
     board = chess.Board(fen)
 
+
     for depth in range(FLAGS.depth + 1):
-      # More reproducible if we clear every time through.
-      engine.configure({"Clear Hash": None})
       sfen = f'{fen}|{depth}'
       if sfen in reference:  # In memory
         ncache_ref += 1
@@ -123,7 +139,17 @@ def main(argv):
       if sfen in db:  # Hmm, maybe should be in memory
         ncache += 1
         continue
+      hack = time.time()
+      #print('\t', sfen)
+      # More reproducible if we clear every time through.
+      #if engine is not None:
+      #engine.quit()
+      engine = open_engine()
+      #engine.configure({"Clear Hash": None})
       multi = engine.analyse(board, chess.engine.Limit(depth=depth), multipv=MULTIPV)
+      engine.quit()
+      engine = None
+      #print('\t', sfen, f'{time.time() - hack:.1f}s')
       if len(multi) > 0:
         nodes += multi[0].get('nodes', 0)
 
