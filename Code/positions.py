0a312f2e50e76509b0a8681a87616c1faab88d4b
Dave Spencer <david.spencerian@gmail.com>
2022-10-07 23:22:40 -0700

    ply flags

--- Code/positions.py
+++ Code/positions.py
@@ -6,10 +6,17 @@ from pprint import pprint
 import time
 import collections
 from util import *
+import resource
 
-MIN_PLY = 19
-MAX_PLY = 60
-MIN_FREQ = 10
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('min_ply', 19, 'Starting ply')
+flags.DEFINE_integer('max_ply', 19, 'Ending ply')
+flags.DEFINE_integer('min_freq', 10, '')
+
 
 def munch_game(game):
   h = game.headers
@@ -20,9 +27,9 @@ def munch_game(game):
   pos = set()
   for ply, move in enumerate(game.mainline_moves()):
     board.push(move)
-    if ply < MIN_PLY:
+    if ply < FLAGS.min_ply:
       continue
-    if ply > MAX_PLY:
+    if ply > FLAGS.max_ply:
       break
     pos.add(simplify_fen(board))
   return pos
@@ -36,18 +43,27 @@ def munch_file(fn):
     if gnum % 25000 == 0:
       dt = time.time() - t1
       print(gnum, f'{100.0 * pct:.2f} {dt:.1f}s', len(counts))
-  dt = time.time() - t1
-  print(fn, len(counts), f'{dt:.1f}s')
+
   return counts
 
 
-counts = collections.Counter()
-for fn in sys.argv[1:]:
-  counts.update(munch_file(fn))
+def main(argv):
+  counts = collections.Counter()
+  for fn in argv[1:]:
+    t1 = time.time()
+    counts.update(munch_file(fn))
+    dt = time.time() - t1
+    print(fn, len(counts), f'{dt:.1f}s')
 
-print('Done: ', len(counts))
+  print('Done: ', len(counts))
+  maxrss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss // 1024 // 1024
+  print('RSS: ', maxrss)
 
-for n, v in counts.items():
-  if v < MIN_FREQ:
-    continue
-  print(f'{n},{v}')
+  for n, v in counts.items():
+    if v < FLAGS.min_freq:
+      continue
+    print(f'{n},{v}')
+
+
+if __name__ == "__main__":
+  app.run(main)
