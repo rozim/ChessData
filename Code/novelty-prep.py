23c060b05c8105ec2279cf8b0516d3b75157436e
Dave Spencer <david.spencerian@gmail.com>
2022-10-09 21:59:46 -0700

    prelim version passes ad hoc tests

--- Code/novelty-prep.py
+++ Code/novelty-prep.py
@@ -1,37 +1,48 @@
-# gen_games
-
-# python pgn-benchmark1.py ../ChessOk.com/mega-clean.pgn
-# 465587 623.4s
-
-
-# /Users/dave/Downloads/pypy-c-jit-106225-39ac625a6e2a-macos_arm64/bin/pypy3 pgn-benchmark1.py ../ChessOk.com/mega-clean.pgn
-# 460000 460000 98.8% 460.0s
-# 465587 465.4s
-#
-# 75% of the earlier time
+# Prepare date -> list of positions to make the next stage of novelty
+# calculations go faster as then we'll be sorted by date and won't ever
+# have to overwrite any data.
 
 import os
 import sys
 import time
-
+import collections
 
 import chess
 import chess.pgn
 
+import sqlitedict
+import json
+
 
 from util import *
 
-
+db = collections.defaultdict(list)
 ng = 0
-t1 = time.time()
+t0 = time.time()
 for fn in sys.argv[1:]:
   print(fn)
-  for gnum, (g, pct) in enumerate(gen_games(fn)):
-    if gnum % 10000 == 0:
-      print(f'{gnum} {ng} {pct*100:.1f}% {time.time()-t1:.1f}s')
-      if gnum >= 30000:
-        break
-    ng += 1
+  for gnum, (game, pct, pos) in enumerate(gen_games_pos(fn)):
+    cur_date = game.headers['Date'].replace('?', '9')
+    if len(cur_date) > 10:
+      cur_date = cur_date[0:10]
+    db[cur_date].append(pos)
+    if gnum % 1000 == 0:
+      print(f'{gnum} {100*pct:.1f}% {time.time()-t0:.1f}s')
+    if gnum > 10000:
+      break
 
-dt = time.time() - t1
-print(f'{ng} {dt:.1f}s')
+print('Persisting')
+sdb = sqlitedict.open('novelty-dates.sqlite',
+                      flag='c',
+                      encode=json.dumps,
+                      decode=json.loads)
+
+for row, (n, v) in enumerate(db.items()):
+  sdb[n] = v
+  if row % 100 == 0:
+    sdb.sync()
+
+sdb.sync()
+
+dt = time.time() - t0
+print(f'{ng} {dt:.1f}s {len(db)} {len(sdb)}')
