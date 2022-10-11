d264ac9b9a6c6a27e57d853a7770c3e6e41ece67
Dave Spencer <david.spencerian@gmail.com>
2022-10-11 16:34:20 -0700

    ran for a long time then thread timeout

--- Code/novelty-dates.py
+++ Code/novelty-dates.py
@@ -1,4 +1,4 @@
-# Generate first date seen of positions.
+#  Generate first date seen of positions.
 import sys
 import os
 import chess
@@ -21,7 +21,7 @@ flags.DEFINE_integer('max_ply', 60, 'Ending ply')
 n_improve = 0
 n_not_improve = 0
 n_add = 0
-n_bad_date = 0
+
 
 DBG = '3rrqk1/1p3ppp/p1bb1n2/3p4/7Q/1PN1PN2/PB3PPP/2RR2K1 b - -'
 
@@ -53,18 +53,12 @@ class NoveltyDb:
     self.d1.sync()
 
 
-def munch_game(game, novelty_db):
-  global n_improve, n_not_improve, n_add, n_bad_date
+def munch_game(game, cur_date, novelty_db):
+  global n_improve, n_not_improve, n_add
 
   h = game.headers
   if 'FEN' in h or 'SetUp' in h:
     return None
-  cur_date = h['Date'].replace('?', '9')
-  if len(cur_date) > 10:
-    n_bad_date += 1
-    cur_date = cur_date[0:10]
-  # assert len(cur_date) == 10, (cur_date, h)
-  # sample bad date: [Date "2022.08.22`"]
 
   board = chess.Board()
   for ply, move in enumerate(game.mainline_moves()):
@@ -78,6 +72,7 @@ def munch_game(game, novelty_db):
         if sfen == DBG:
           print('IMPROVE ', db_date, cur_date)
         n_improve += 1
+        assert False, 'should be impossible now'
         novelty_db[sfen] = cur_date
       else:
         n_not_improve += 1
@@ -88,31 +83,51 @@ def munch_game(game, novelty_db):
       n_add += 1
 
 
-def munch_file(fn, novelty_db):
-  global n_add, n_improve, n_not_improve, n_bad_date
-  t1 = time.time()
-  for gnum, (g, pct) in enumerate(gen_games(fn)):
-    munch_game(g, novelty_db)
-    if gnum % 25000 == 0:
-      dt = time.time() - t1
-      n1 = len( novelty_db.d1)
-      n2 = len( novelty_db.d2)
-      print(f'\t{gnum} {100.0 * pct:.2f}% {dt:.1f}s add={n_add} improve={n_improve} | {n_not_improve} d1={n1} d2={n2}')
-      if n2 > 10000000:
-        print('[flush]')
-        novelty_db.flush()
-
 
 def main(argv):
-  global n_add, n_improve, n_not_improve, n_bad_date
+  global n_add, n_improve, n_not_improve
+
+  assert len(argv[1:]) == 1
+
+  t0 = time.time()
+
+  dates_db = sqlitedict.open('novelty-prep.sqlite',
+                              flag='r',
+                              encode=json.dumps,
+                              decode=json.loads)
+
   novelty_db = NoveltyDb()
 
-  for fn in argv[1:]:
-    t1 = time.time()
-    munch_file(fn, novelty_db)
-    novelty_db.flush()
-    dt = time.time() - t1
-    print('fn: ', fn, f'{dt:.1f}s add={n_add} improve={n_improve} : {n_not_improve}')
+  pgn_fn = argv[1:][0]
+  pgn_f = open(pgn_fn, 'r', encoding='utf-8', errors='replace')
+
+  ng = 0
+  dates = sorted(dates_db.keys())
+  for di, date in enumerate(dates):
+    if len(date) != 10:
+      print('BAD LEN: ', date)
+      continue
+    positions = dates_db[date]
+    pcd = 100.0 * di / len(dates)
+    if di % 10 == 0:
+      print(f'{di:6d} {date:10s} {pcd:3.1f}% {time.time() - t0:4.1f}s  np={len(positions)} ng={ng} add={n_add} imp={n_improve} ~imp={n_not_improve}')
+    for pos in dates_db[date]:
+      pgn_f.seek(pos, 0)
+      game = chess.pgn.read_game(pgn_f)
+
+      munch_game(game, date, novelty_db)
+
+      fens = gen_fens(game)
+      ng += 1
+      if ng % 10000 == 0:
+        print('\t', 'ng=', ng, f'{time.time() - t0:.1f}s #fens={len(list(fens))}')
+
+      if ng % 100000 == 0:
+        t1 = time.time()
+        print('FLUSH')
+        novelty_db.flush()
+        dt = time.time() - t1
+        print(f'FLUSHED {dt:.1f}s')
 
   maxrss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss // 1024 // 1024
   print('RSS: ', maxrss)
@@ -121,9 +136,8 @@ def main(argv):
   print('d2          : ', len(novelty_db.d2))
 
   print('Improve     : ', n_improve)
-  print('Not Improve : ', n_improve)
+  print('Not Improve : ', n_not_improve)
   print('Add         : ', n_add)
-  print('Bad date    : ', n_bad_date)
 
 
 if __name__ == "__main__":
