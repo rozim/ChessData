9eeade981941ad572eab99f1dea016f1de28a617
Dave Spencer <david.spencerian@gmail.com>
2022-10-14 15:43:17 -0700

    d24 on new random sample

--- Code/parallelize.sh
+++ Code/parallelize.sh
@@ -6,7 +6,8 @@ bn=$(basename $fn)
 out_dir=/tmp/$$
 mkdir -p ${out_dir}
 split -d -l 10 -a 3 ${fn} ${out_dir}/${bn}
-ls ${out_dir}/${bn}* | xargs -P 5 -I % -t  python -u fen-analysis-db.py --fen=% --output=%.sqlite --depth=32 --reference=../Analysis/nn-ad9b42354671/pv1-deep/deep.sqlite
+ls ${out_dir}/${bn}* | xargs -P 6 -I % -t  python -u fen-analysis-db.py --fen=% --output=%.sqlite --depth=24
+# --reference=../Analysis/nn-ad9b42354671/pv1-deep/deep.sqlite
 
 # lines=$(wc -l ${fn})
 # nice python -u fen-analysis-db.py --fen=../Release/2022-10-05/mega-clean-sample-1000.txt --depth=31 --output=../Analysis/nn-ad9b42354671/pv1-deep/deep.sqlite >& deep31.shh
