fae0fa56136e2fd1e8564a7ea59ef5854b50dc03
Dave Spencer <david.spencerian@gmail.com>
2022-10-14 07:57:45 -0700

    look for balanced positions

--- Code/select-random-positions.py
+++ Code/select-random-positions.py
@@ -19,7 +19,10 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('n', 10, '')
 flags.DEFINE_integer('min_ply', 2, '')
+flags.DEFINE_integer('root_score', 150, 'Position must be +/- this so they are somewhat balanced')
 
+HASH = 512
+THREADS = 1 # reproducible
 
 def main(argv):
   sampled = [''] * FLAGS.n
@@ -29,12 +32,17 @@ def main(argv):
   fn_random = random.random
   fn_randint = random.randint
   fn_choice = random.choice
+
+  engine = chess.engine.SimpleEngine.popen_uci('stockfish')
+  engine.configure({"Hash": HASH})
+  engine.configure({"Threads": THREADS})
+
   for fn in argv[1:]:
-    print('FN: ', fn)
+    #print('FN: ', fn)
     for gnum, (game, pct) in enumerate(gen_games(fn)):
       if gnum % 5000 == 0:
         dt = time.time() - t0
-        print(gnum, f'{dt:.1f}s')
+        #print(gnum, f'{dt:.1f}s')
 
       fens = list(gen_fens(game))
       if len(fens) < FLAGS.min_ply:
@@ -47,16 +55,33 @@ def main(argv):
       elif fn_random() < (FLAGS.n / (1.0 + gnum)):
         slot = fn_randint(0, FLAGS.n - 1)
 
-      if slot >= 0:
-        fen = fn_choice(fens)
-        sfen = ' '.join(fen.split(' ')[0:4])
-        if sfen not in sampled:
-          sampled[slot] = sfen
+      if slot < 0:
+        continue
 
-  print(f'short: {short}')
-  print(f'good: {len(sampled)}')
-  print()
+      fen = fn_choice(fens)
+
+      engine.configure({"Clear Hash": None})
+      m = engine.analyse(chess.Board(fen), chess.engine.Limit(depth=0))
+      is_mate, score = simplify_score2(m['score'])
+      if is_mate:
+        # print('MATE: ', fen)
+        continue
+      elif score > FLAGS.root_score or score < -FLAGS.root_score:
+        # print('RANGE: ', fen, score)
+        continue
+      else:
+        # print('GOOD: ', fen, score)
+        pass
+
+      sfen = ' '.join(fen.split(' ')[0:4])
+      if sfen not in sampled:
+        sampled[slot] = sfen
+
+  #print(f'short: {short}')
+  #print(f'good: {len(sampled)}')
+  #print()
   print('\n'.join(sampled))
+  engine.quit()
 
 
 
