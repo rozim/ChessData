ad7a74850e9e94eb2bee1e9c0a53be4ba301b9fd
Dave Spencer <david.spencerian@gmail.com>
2022-10-14 19:56:25 -0700

    refactor to try to be more efficient by not going thru all games

--- Code/select-random-positions.py
+++ Code/select-random-positions.py
@@ -25,62 +25,51 @@ HASH = 512
 THREADS = 1 # reproducible
 
 def main(argv):
-  sampled = [''] * FLAGS.n
-
-  short = 0
   t0 = time.time()
-  fn_random = random.random
-  fn_randint = random.randint
   fn_choice = random.choice
 
   engine = chess.engine.SimpleEngine.popen_uci('stockfish')
   engine.configure({"Hash": HASH})
   engine.configure({"Threads": THREADS})
 
+  locations = []
   for fn in argv[1:]:
     #print('FN: ', fn)
-    for gnum, (game, pct) in enumerate(gen_games(fn)):
-      if gnum % 5000 == 0:
-        dt = time.time() - t0
-        #print(gnum, f'{dt:.1f}s')
+    for _, _, pos in gen_games_pos(fn):
+      locations.append((fn, pos))
+  random.shuffle(locations)
+  sys.stderr.write(f'LOCS: {len(locations)}\n')
 
-      fens = list(gen_fens(game))
-      if len(fens) < FLAGS.min_ply:
-        short += 1
-        continue
-      fens = fens[:-1] # final position may be mate so don't pick it
-      slot = -1
-      if gnum < FLAGS.n:
-        slot = gnum
-      elif fn_random() < (FLAGS.n / (1.0 + gnum)):
-        slot = fn_randint(0, FLAGS.n - 1)
+  already = set()
+  good = 0
+  for gnum, (fn, pos) in enumerate(locations):
+    if gnum % 1000 == 0:
+      sys.stderr.write(f'{gnum}\n')
+    f = open(fn, 'r', encoding='utf-8', errors='replace')
+    f.seek(pos, 0)
+    game = chess.pgn.read_game(f)
+    f.close()
 
-      if slot < 0:
-        continue
+    fens = list(gen_fens(game))
+    if len(fens) < FLAGS.min_ply:
+      continue
+    fen = fn_choice(fens[:-1]) # final position may be mate so don't pick it
 
-      fen = fn_choice(fens)
+    engine.configure({"Clear Hash": None})
+    m = engine.analyse(chess.Board(fen), chess.engine.Limit(depth=0))
+    is_mate, score = simplify_score2(m['score'])
+    if is_mate:
+      continue
+    elif score > FLAGS.root_score or score < -FLAGS.root_score:
+      continue
+    sfen = ' '.join(fen.split(' ')[0:4])
+    if sfen not in already:
+      print(sfen)
+      already.add(sfen)
+      good += 1
+      if good >= FLAGS.n:
+        break
 
-      engine.configure({"Clear Hash": None})
-      m = engine.analyse(chess.Board(fen), chess.engine.Limit(depth=0))
-      is_mate, score = simplify_score2(m['score'])
-      if is_mate:
-        # print('MATE: ', fen)
-        continue
-      elif score > FLAGS.root_score or score < -FLAGS.root_score:
-        # print('RANGE: ', fen, score)
-        continue
-      else:
-        # print('GOOD: ', fen, score)
-        pass
-
-      sfen = ' '.join(fen.split(' ')[0:4])
-      if sfen not in sampled:
-        sampled[slot] = sfen
-
-  #print(f'short: {short}')
-  #print(f'good: {len(sampled)}')
-  #print()
-  print('\n'.join(sampled))
   engine.quit()
 
 
