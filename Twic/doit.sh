85afeea51528703a8ccffbdbe9617642a35259b0
Dave Spencer <david.spencerian@gmail.com>
2023-05-29 22:30:27 -0700

    undo accidental edits

--- Twic/doit.sh
+++ Twic/doit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-for i in {1203..1259}
+for i in {1458..1490}
 do
     echo wget http://www.theweekinchess.com/zips/twic${i}g.zip
     echo unzip -q twic${i}g.zip
     echo rm twic${i}g.zip
 done
-# 
